2b5f3d45763383194187b8f25ac6dec1db07e91c
edsonlisboadev <edsonlisboadesenvolvedor@gmail.com>
2026-05-26 09:55:15 -0300

    FEAT: Adição de torta

--- atelie-dos-sabores/src/components/Hero/index.tsx
+++ atelie-dos-sabores/src/components/Hero/index.tsx
@@ -48,7 +48,12 @@ export default function Hero() {
           <div className={styles.imagePlaceholder}>
             <div className={styles.plateRing} />
             <div className={styles.plateInner}>
-              <span className={styles.plateEmoji}>🥐</span>
+              <div className={styles.plateInner}>
+              <img
+                src="/img/torta.png"
+                alt="Salgados artesanais"
+                className={styles.plateImage}/>
+                </div>
             </div>
           </div>
 
