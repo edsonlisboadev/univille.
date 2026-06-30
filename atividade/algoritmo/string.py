3841d73c542d220f6885d8df874e50fe2bf0b55c
edsonlisboadev <edsonlisboadesenvolvedor@gmail.com>
2026-06-30 11:47:45 -0300

    Adicionando no Git Pages

---
+++ atividade/algoritmo/string.py
@@ -0,0 +1,16 @@
+tex = "Hello World!"
+print(len(tex))
+
+
+a = "xoxo"
+b = "XOXO"
+#tem x no a 
+print("x" in a)
+# a é igual b minusculo
+print(a.casefold() == b.casefold())
+# a e b n são iguais se tiverem em maiusculo e minusculo
+print(a == b)
+#substitui o primeiro pelo segundo 
+print(tex.replace('l', 'seu filha da puta'))
+# não sei para oq serve
+print(tex.split(' '))
